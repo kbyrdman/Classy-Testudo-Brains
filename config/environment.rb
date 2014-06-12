f7e2673c644510b0c402882bb4bafe338db7149a
Kyle Byrd <kbyrd@jibe.com>
2014-06-12 01:02:35 -0400

    configured mongo

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ $LOAD_PATH << File.join(APP_ROOT, 'lib') unless $LOAD_PATH.include?(File.join(AP
 
 require 'capybara/poltergeist'
 require 'classy_testudo_brains'
+require 'mongo'
+require 'mongo_mapper'
+
+# => Configure the MongoDB Connection
+File.open(File.join(APP_ROOT, 'config', 'mongo.yml'), 'r') do |f|
+  cfg = (YAML::load(f.read)).symbolize_keys
+  MongoMapper.connection = Mongo::Connection.new(cfg[:host] || 'localhost', cfg[:port] || 27017)
+  MongoMapper.database = cfg[:database] || "umd_waitlist"
+  if (cfg[:username] and cfg[:password])
+    MongoMapper.database.authenticate(cfg[:username], cfg[:password])
+  end
+end
+
+require "models"
